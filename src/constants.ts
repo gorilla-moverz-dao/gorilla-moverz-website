eff3b2f0d719e5afbb96d6aba8a7fec990e7f02a
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2024-08-15 23:08:25 +0200

    Fix movement testnet config (except indexer)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { Network } from "@aptos-labs/ts-sdk";
+
 export const SUPRA_API_URL = "https://pinphweythafvrejqfgm.supabase.co";
 export const SURPA_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpbnBod2V5dGhhZnZyZWpxZmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3MDE5NzAsImV4cCI6MjAzMzI3Nzk3MH0.jUdXMHfDxyALSRdw4FW5Zjqgsh9vq6bp8dNUtstBcYw";
@@ -5,8 +7,8 @@ export const MODULE_ADDRESS =
   "8f8045299964708acb332287ef304ee5be081b790a7a21e05647090e8ac28cdc";
 export const FULL_NODE = "https://aptos.testnet.suzuka.movementlabs.xyz/v1";
 export const FARM_COLLECTION_ID =
-  "0xba47e8a4111d53d81773e920b55c4152976a47ea4b002777cd81e8eb6ed9e4e2";
-export const NETWORK = "https://aptos.testnet.suzuka.movementlabs.xyz/v1";
+  "0xeb2401e0658b67f4a11d48ce6efc61788858863a5a29e25b78afc9a3a8f44cff";
+export const NETWORK = Network.CUSTOM;
 export const BANANA_CONTRACT_ADDRESS =
   "0x4d92915d90fe5b2a99c942d3f249493e2ff57e835b5ce8f88c178444641828c0";
 export const EXCLUDE_LEADERBOARD = ["0x" + MODULE_ADDRESS];
--- src/services/movement-client.ts
+++ src/services/movement-client.ts
@@ -3,6 +3,8 @@ import { NETWORK } from "../constants";
 
 const aptosConfig = new AptosConfig({
   network: NETWORK,
+  fullnode: "https://aptos.testnet.suzuka.movementlabs.xyz/v1",
+  indexer: "https://aptos.testnet.suzuka.movementlabs.xyz/indexer",
 });
 const movementClient = new Aptos(aptosConfig);
 
