735a9e47e58cc2641384784db35c9cd7bd7d212a
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2024-08-22 22:41:21 +0200

    Add instructions before redirecting to discord

--- supabase/migrations/20240822085456_nft_index.sql
+++ supabase/migrations/20240822085456_nft_index.sql
@@ -1,2 +1,2 @@
 CREATE INDEX ON "public"."banana_farm_nfts" USING btree ("nft_number");
-CREATE INDEX ON "public"."banana_farm_nfts" USING btree ("banana_farmer_collection_id");
+CREATE INDEX ON "public"."banana_farm_nfts" USING btree ("collection_id");
