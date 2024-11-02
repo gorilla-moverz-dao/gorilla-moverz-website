804ac34de0a3c5da39c5c8db1759629178288955
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2024-11-02 22:24:42 +0100

    Deploy to Porto Testnet

--- src/constants.ts
+++ src/constants.ts
@@ -2,10 +2,13 @@ import { Network } from "@aptos-labs/ts-sdk";
 
 export const SUPRA_API_URL = import.meta.env?.VITE_SUPRA_API_URL;
 export const SURPA_ANON_KEY = import.meta.env?.VITE_SUPRA_ANON_KEY;
-export const MODULE_ADDRESS = "323f7c341d2fdfda5c5dfefb1b51cad9063a0ad599fdc716ba12fea528510c5b";
+export const MODULE_ADDRESS = "f161ef37562d16b9f4e0aac020cdc4e902a871effec1f706eff738546f3e8e73";
 export const FULL_NODE = "https://aptos.testnet.porto.movementlabs.xyz/v1";
-export const FARM_COLLECTION_ID = "0xba47e8a4111d53d81773e920b55c4152976a47ea4b002777cd81e8eb6ed9e4e2";
+export const FARM_COLLECTION_ID = "0xb0304659f0f67035a59878148092ed5b2426b46489a01bc5cc4d906d3ab9d853";
 export const NETWORK = Network.CUSTOM;
-export const BANANA_CONTRACT_ADDRESS = "0x4d92915d90fe5b2a99c942d3f249493e2ff57e835b5ce8f88c178444641828c0";
-export const EXCLUDE_LEADERBOARD = ["0x" + MODULE_ADDRESS];
+export const BANANA_CONTRACT_ADDRESS = "0xcbffa124c1b5ffb7efc79a95612b95ea20bc7a1b75d045ca94a7263a5c3c4b40";
+export const EXCLUDE_LEADERBOARD = [
+  "0x" + MODULE_ADDRESS,
+  "0x2c9ab467113c6c3c1a990ae0eec00d4b8d0012278079c3c7705b7cdf57264d14",
+];
 export const FOUNDERS_COLLECTION_ID = "0x323f7c341d2fdfda5c5dfefb1b51cad9063a0ad599fdc716ba12fea528510c5b";
